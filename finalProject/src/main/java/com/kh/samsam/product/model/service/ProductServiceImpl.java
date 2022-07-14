1f54e8f7a1e6239509a22b0dc4e581c45fa30de4
user1 <user1@host.docker.internal>
2022-07-14 15:56:43 +0900

    sdsdsd

---
+++ finalProject/src/main/java/com/kh/samsam/product/model/service/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package com.kh.samsam.product.model.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.samsam.common.model.vo.Category;
+import com.kh.samsam.product.model.dao.ProductDao;
+import com.kh.samsam.product.model.vo.Product;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+	@Autowired
+	private ProductDao productDao;
+	@Autowired SqlSessionTemplate sqlSession;
+	
+	
+	
+	@Override
+	public int insertProduct(Product p) {
+		return productDao.insertProduct(sqlSession,p);
+	}
+
+
+
+	@Override
+	public ArrayList<Category> selectCategoryList(int categoryNo) {
+		return productDao.selectCategoryList(sqlSession,categoryNo);
+	}
+
+}
